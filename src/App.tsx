5ba87ef567f3cc6b2b25524ecd6624edb3e2230d
Nelson Eldoro <nelsoneldoro@gmail.com>
2021-09-25 14:05:42 -0300

    chore: config ts css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css'; // Import css modules stylesheet as styles
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles.app}>
+      <header>
+        <img src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
