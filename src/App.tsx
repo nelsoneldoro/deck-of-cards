f1c72935a4813df03760eb175edb8a82484fa0e6
Nelson Eldoro <nelsoneldoro@gmail.com>
2021-09-25 23:03:29 -0300

    refactor(App): explore enum with routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,24 @@ import Form from './pages/Form';
 import Deck from './pages/Deck';
 import Layout from './components/Layout';
 
+export enum Routes {
+  root = '/',
+  new = '/deck/new',
+  deck = '/deck/:id',
+}
+
 function App() {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route exact path="/deck/new">
+          <Route exact path={Routes.new}>
             <Form />
           </Route>
-          <Route exact path="/deck/:id">
+          <Route exact path={Routes.deck}>
             <Deck />
           </Route>
-          <Redirect exact from="/" to={'/deck/new'} />
+          <Redirect exact from={Routes.root} to={Routes.new} />
         </Switch>
       </Layout>
     </Router>
